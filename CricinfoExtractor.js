99077ea507458a63d8139a5d8d9d366d511d646a
Parmeet Kaur Narang <56155933+parmeetnarang2812@users.noreply.github.com>
2021-10-22 17:40:33 +0530

    -

--- CricinfoExtractor.js
+++ CricinfoExtractor.js
@@ -2,7 +2,7 @@
     ->The purpose of this project is to extract the information of Worldcup 2019 from Cricinfo
       and present that in the perform of excel and pdf scorecards.
     ->The real purpose is to learn how to extract information and get experienced with javascript. 
-    ->A very good reason to make this project is to have good fun with webscrapping. 
+    ->A very good reason to make this project is to have good fun with webscraping. 
 */
 
 /*
